76167977d86e95f256c2f2ccf3934a0309b6865b
erico07181 <enriquearico@gmail.com>
2023-02-19 11:45:16 -0700

    Navbar styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,16 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Waitlist from "../Waitlist/Waitlist";
 import ContactUs from "../ContactUs/ContactUs";
+import Nav from "../Navbar/Nav";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
     <div className="App">
+      <Nav />
       <Waitlist />
       <ContactUs />
     </div>
   );
 }
-
-export default App;
